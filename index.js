d63d8b197c984fcd95da64318f1a482a2327d75d
Sumonche016 <sumonche016@gmail.com>
2022-06-30 20:42:45 +0600

    db-cnnected

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express()
 
 //midew3are 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ndjoo.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('db connected')
+    // perform actions on the collection object
+    client.close();
+});
+
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('runnig the server')
